678a06f10f20f3c6a927ebeff94785ccf23c5930
techflag <562635045@qq.com>
2026-09-17 15:42:39 +0800

    feat: complete project list filtering

--- packages/plugins/projects/src/index.ts
+++ packages/plugins/projects/src/index.ts
@@ -6,4 +6,11 @@ import { registerProjectConnection } from './remote/connection-api.js';
 export * from './services/project-manager.js'; export * from './storage/domain.js';
 export const name='workdsh-plugin-projects';
 export const inject=['storageDomain','connection','workdshIdentity'];
-export async function apply(ctx:Context){await ctx.plugin(ProjectManager);registerProjectConnection(ctx);}
+export async function apply(ctx:Context){
+  await ctx.plugin(ProjectManager);
+  await ctx.plugin({
+    name:'workdsh-projects-integration',
+    inject:[...inject,'workdshProjects'],
+    apply(integration:Context){registerProjectConnection(integration);},
+  });
+}
